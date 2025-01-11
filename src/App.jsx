d78ad8f542f7bdd008a9590c3491dc29827470c8
Matt <mwelc64@yahoo.com>
2025-01-11 07:29:00 -0500

    connected aboutme.css to Aboutme component

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import AboutMe from './pages/AboutMe';
 import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
 import Resume from './pages/Resume';
-import './styles.css';
+import './styles/styles.css';
 
 
 function App() {
--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import '../styles/aboutme.css';
+
 
 function AboutMe() {
     return (
